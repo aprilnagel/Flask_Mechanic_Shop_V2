b543b3fd59051b640b5c175c6fc701b198912bc0
April Nagel <april.n.nagel@gmail.com>
2026-02-02 16:12:10 -0600

    updated customer schema because of the loading issue

--- Backend/app/blueprints/customers/schemas.py
+++ Backend/app/blueprints/customers/schemas.py
@@ -1,14 +1,22 @@
 from app.extensions import ma
 from app.models import Customers
 
-class CustomerSchema(ma.SQLAlchemyAutoSchema):
-    password = ma.String(load_only=True)   
+class CustomerSchema(ma.Schema):
+    password = ma.String(load_only=True)
 
     class Meta:
-        model = Customers
-        load_instance = True
-        include_fk = True
-        dump_only = ("id",)               
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "phone",
+            "address",
+            "role",
+            "password"
+        )
+        dump_only = ("id",)
+         
         
 customer_schema = CustomerSchema()
 customers_schema = CustomerSchema(many=True)
