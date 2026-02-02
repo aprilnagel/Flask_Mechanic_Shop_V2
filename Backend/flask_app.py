eafc5b21f88c9bfc46503c1729fafb9824fa05d6
April Nagel <april.n.nagel@gmail.com>
2026-02-02 13:28:37 -0600

    apparently CORS wasnt installed

--- Backend/flask_app.py
+++ Backend/flask_app.py
@@ -1,7 +1,9 @@
 from app.models import db
 from app import create_app
+from flask_cors import CORS   # <-- ADD THIS
 
 app = create_app("ProductionConfig")
+CORS(app)                     # <-- AND THIS
 
 @app.route("/health")
 def health():
