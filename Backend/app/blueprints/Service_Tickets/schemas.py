8ed65cc4feb1c6f6cf1d39fef572bd23682a66e1
April Nagel <april.n.nagel@gmail.com>
2026-02-02 20:20:30 -0600

    updated service ticket schema

--- Backend/app/blueprints/Service_Tickets/schemas.py
+++ Backend/app/blueprints/Service_Tickets/schemas.py
@@ -1,11 +1,11 @@
 from marshmallow import fields
 from app.extensions import ma
 from app.models import Service_Tickets
-from app.blueprints.mechanics.schemas import MechanicSchema
+from app.blueprints.mechanics.schemas import mechanic_schema
 
 class ServiceTicketSchema(ma.SQLAlchemyAutoSchema):
     mechanics = fields.List(
-        fields.Nested(MechanicSchema),
+        fields.Nested(mechanic_schema),
         attribute="mechanics_service_tickets"
     )
 
